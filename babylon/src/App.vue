8f45e47dde59475baf425a35a7a8441b927a96b3
mrx <1320504268@qq.com>
2024-05-13 23:01:09 +0800

    init

---
+++ babylon/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <canvas ref="canvas"></canvas>
+  </div>
+</template>
+<script setup>
+import * as BABYLON from 'babylonjs'
+
+</script>
+
+<style scoped>
+canvas {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
